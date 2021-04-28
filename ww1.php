689ee56894c988a476d49fa04baf3cc7dfff69cd
ManyaSmriti19 <72989251+ManyaSmriti19@users.noreply.github.com>
2021-04-28 07:52:37 +0530

    Update and rename ww.php to ww1.php

--- ww1.php
+++ ww1.php
@@ -628,7 +628,7 @@ body {
         <div class="row heading">
           <div class="col-sm-3 col-xs-3 heading-avatar">
             <div class="heading-avatar-icon">
-              <img src="https://bootdey.com/img/Content/avatar/avatar3.png">
+              <img src="https://bootdey.com/img/Content/avatar/avatar8.png">
             </div>
           </div>
          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -668,7 +668,7 @@ if ($result->num_rows > 0) {
               echo "<div class='row'>";
                 echo "<div class='col-sm-8 col-xs-8 sideBar-name'>";
                   echo "<span class='name-meta' id='contact'>";
-                   $src = "/proj1/ww.php?email=".$_GET['email']."&user2=".$row['email']; echo "<a href=".$src.">"; echo $row['email']; echo "</a>";
+                   $src = "/proj1/ww1.php?email=".$_GET['email']."&user2=".$row['email']; echo "<a href=".$src.">"; echo $row['email']; echo "</a>";
                 echo "</span>";
                 echo "</div>";
                
@@ -684,7 +684,7 @@ if ($result->num_rows > 0) {
       <div class="row heading">
         <div class="col-sm-2 col-md-1 col-xs-3 heading-avatar">
           <div class="heading-avatar-icon">
-            <img src="https://bootdey.com/img/Content/avatar/avatar6.png">
+            <img src="https://bootdey.com/img/Content/avatar/avatar3.png">
           </div>
         </div>
 	
@@ -705,15 +705,15 @@ if ($result->num_rows > 0) {
       <div class="row message" id="conversation">
         <div class="row message-previous">
           <div class="col-sm-12 previous">
-            <a onclick="previous(this)" id="smriti" name="20">
+            <a onclick="previous(this)" id="smriti " name="20">
             Share Files!<br>
             </a><br>
 	    <b>
       <?php
 $conn= new mysqli("localhost","root","","secure_chat");
 $b = $_GET['email'];
-$a= $_GET['email'];
-  $sql = "select file from ".$u2. " where name ='$a'";
+$a= $_GET['user2'];
+  $sql = "select file from ".$u2. " where name ='$b'";
   $result = $conn -> query($sql);
   if ($result-> num_rows > 0) {
     while($row = $result -> fetch_assoc()) {
@@ -740,8 +740,270 @@ $file_size = $_FILES['file']['size'];
 $file_tem_loc = $_FILES['file']['tmp_name'];
 $file_store = "cover1/".$file_name;
 $file= $file_name;
-$sql = "insert into ".$b."(name, file) values ('$u2','$file')";
+$sql = "insert into ".$a."(name, file) values ('$u2','$file')";
 $conn -> query($sql);
+$p=101;
+
+$q=103;
+
+$r=107;
+
+$s=109;
+
+$n=$p*$q;
+
+$z=$r*$s;
+
+$phin=($p-1)*($q-1);
+
+$phiz=($r-1)*($s-1);
+
+// Function to print primes for e n 
+/*$count=0;
+$arrayp=array(0,1);
+$isit=0;*/
+/*
+// Driver Code
+$nn = $phin; 
+for ($i = 2; $i <= $nn; $i++) 
+{
+    if($nn<=1)
+    $isit=0;
+    for($i=2;$i<$nn;$i++)
+    {
+        if($nn%$i==0)
+        $isit=1;
+    }
+    if ($isit==1)
+    {
+        $arrayp[$count]=$i;
+        $count++;
+    }
+} 
+$cc=count($arrayp);
+/*for($i=0;$i<$cc;$i++)
+{
+    echo $arrayp[$i];
+    echo("<br>");
+}
+$vale=0;
+for($i=0;$i<$cc;$i++)
+{
+    if(($arrayp[$i]>1) && ($arrayp[$i]<$n))
+    {
+        if(gmp_gcd($arrayp[$i],$phin)==1)
+        {
+            $e=$arrayp[$i];
+            echo $e;
+            $vale=$e;
+        }
+    }
+}*/
+for($i=$phin;$i>0;$i--)
+{
+    if(gmp_gcd($i,$phin)==1){
+        $e=$i;
+        break;
+    }
+}
+
+for($i=$phiz;$i>0;$i--)
+{
+    if(gmp_gcd($i,$phiz)==1){
+        $g=$i;
+        break;
+    }
+}
+
+//sdsd
+//echo $vale;
+//echo"<br>";
+// Function to print primes for g z
+/*$count=0;
+$arrayp=array(0,1);
+$isit=0;
+// Driver Code
+$ng = $phiz; 
+for ($i = 2; $i <= $ng; $i++) 
+{
+    if($n<=1)
+    $isit=0;
+    for($i=2;$i<$ng;$i++)
+    {
+        if($ng%$i==0)
+        $isit=1;
+    }
+    if ($isit==1)
+    {
+        $arrayp[$count]=$i;
+        $count++;
+    }
+} 
+$cc=count($arrayp);
+/*for($i=0;$i<$cc;$i++)
+{
+    echo $arrayp[$i];
+    echo("<br>");
+}
+for($i=0;$i<$cc;$i++)
+{
+    if(($arrayp[$i]>1) && ($arrayp[$i]<$z))
+    {
+        if(gmp_gcd($arrayp[$i],$phiz)==1)
+        {
+            $g=$arrayp[$i];
+        }
+    }
+}*/
+//sdsd
+//echo $g;
+//echo"<br>";
+//$egmp=strval($e);
+//$phingmp=strval($phin);
+//$ggmp=strval($g);
+//$phizgmp=strval($phiz);
+$d=gmp_invert($e,$phin);
+
+$t=gmp_invert($g,$phiz);
+
+
+$dp=$d % ($p-1);
+$dq=$d % ($q-1);
+$dr=$d % ($r-1);
+$ds=$d % ($s-1);
+//public keys: (e,n),(g,z)
+//private key:(t,z,dp,dq,dr,ds)
+$myfile = fopen("file11.txt", "r") or die("Unable to open file!");
+//echo fread($myfile,filesize("file11.txt"));
+$redf=fread($myfile,filesize("file11.txt"));
+$M=$redf;
+fclose($myfile);
+$count=strlen($M);
+//echo $count."<br>";
+$unpak=unpack("C*",$M);
+/*
+for($i=1;$i<($count+1);$i++)
+{   
+    echo $unpak[$i];
+    echo "<br>";
+    //$unpak[$i]=decbin($unpak[$i]);
+    //echo"<br>";
+}*/
+
+$cipher="";
+$binz=strval(decbin($z));
+$cz=strlen($binz);
+for($i=1;$i<($count+1);$i++)
+{
+    $C1=bcpowmod(strval($unpak[$i]),strval($e),strval($n));
+    $C=intval(bcpowmod($C1,strval($g),strval($z)));
+    $cipher.=str_pad(decbin($C),$cz,"0", STR_PAD_LEFT);
+    //echo"<br>";
+    //echo $C."=C<br>";
+}
+
+//variable M is message in integer form
+//$M=113;
+
+
+$myfile = fopen($file, "w") or die("Unable to open file!");
+$txt = $cipher;
+fwrite($myfile, $txt);
+
+fclose($myfile);
+
+//$count=strlen($M);
+//encryption
+//$C1= pow($M,$e) % $n;
+//$C= pow($C1,$g) % $z;
+//$C=pow($M,$e)%$n;
+//echo $C." =c ";
+//echo"<br>";
+$prikeyar1=array($t,$z);
+$prikeyar2=array($d,$n);
+$pubkeyar1=array($e,$n);
+$pubkeyar2=array($g,$z);
+$privkey=join($prikeyar1);
+$pubkey=join($pubkeyar1);
+
+//SHA
+//hash ( 'sha512' , $C [, bool $raw_output = FALSE ] ) : string
+$hashed = hash('sha512', $C);
+/*
+$privkey1=openssl_pkey_get_private("file://C:\Users\HP\Downloads\openssl-toolkit\server.pem");
+openssl_sign($C,$signature,$privkey1);
+//decryption
+$pubkey1=openssl_pkey_get_public("file://C:\Users\HP\Downloads\openssl-toolkit\server.crt");
+$ok=openssl_verify($C,$signature,$pubkey1);
+if ($ok==1)
+{
+    echo"Signature is good<br>";
+}
+elseif($ok==0)
+{
+    echo"Signature is bad<br>";
+}
+else
+{
+    echo"ugly, eroor checking signature<br>";
+}
+*/
+
+$cipher_db=$cipher;
+$cipher_arr= str_split($cipher_db,14);
+
+$dec_m="";
+for($i=0;$i<count($cipher_arr);$i++){
+    $C=bindec($cipher_arr[$i]);
+    /*echo"<br>";
+    echo $C;*/
+   
+    $M1=bcpowmod($C,strval($g),strval($z));
+    $M=intval(bcpowmod($M1,strval($e),strval($n)));
+
+    
+    // $Cp= $C1 % $p;
+    // $Cq= $C1 % $q;
+    // $Cr= $C1 % $r;
+    // $Cs= $C1 % $s;
+    // $dp= $d % ($p-1);
+    // $dq= $d % ($q-1);
+    // $dr= $d % ($r-1);
+    // $ds= $d % ($s-1);
+    // $mp= bcpowmod(strval($Cp),strval($dp),strval($p));
+    // echo $mp;
+    // $mq= bcpowmod(strval($Cq),strval($dq),strval($q));
+    // $mr= bcpowmod(strval($Cr),strval($dr),strval($r));
+    // $ms= bcpowmod(strval($Cs),strval($ds),strval($s));
+    // echo $mp;
+    
+    $char= chr($M);
+   
+    $dec_m.=$char;
+    // echo $dp;
+    // echo"<br>";
+    // echo $dq;
+    // echo"<br>";
+    
+    // echo $dr;
+    // echo"<br>";
+    // echo $ds;
+    // echo"-----<br>";
+
+
+    // echo $char;
+    
+}
+// echo $dec_m;
+
+
+//$message=pow($C,$d)%$n;
+//echo $message." mm ";
+$hashed2=hash('sha512',$C);
+
+
+//echo($mp.$mq.$mr.$ms);
+
 move_uploaded_file($file_tem_loc, $file_store);
 }
 ?><p id="p1" align="right"></p></b>
